56406b7218b68ce83f6e22ae6f7951ffd1462ec4
Muttahar Sheikh <153789569+muttahar123@users.noreply.github.com>
2025-07-17 10:44:47 +0500

    db connected

--- src/index.js
+++ src/index.js
@@ -1,5 +1,49 @@
-import express from "express"
+
+import connectDB from "./db/index.js";
+import dotenv from "dotenv";
+
+dotenv.config({
+  path: "./env"
+})
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 const app = express()
+(async ()=>{
+  try{
+  await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+  app.on("error", (error)=>{
+    console.log("ERROR:", error);
+    throw error;
+  }) 
+  app.listen(port, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`)
+})
+} catch(error){
+    console.error("Error: ", error)
+    throw error
+  }
+})()
+
 const port = 3000
 
 app.get('/', (req, res) => {
@@ -9,6 +53,4 @@ app.get('/login', (req , res) => {
     res.send('this is login page')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+*/
